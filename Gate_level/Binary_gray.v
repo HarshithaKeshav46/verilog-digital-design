9e3e2820bfe72a80d706b25677abe8920d8560dd
HarshithaKeshav46 <harshithakeshav909@gmail.com>
2026-04-19 17:22:28 +0000

    binary to gray

--- Gate_level/Binary_gray.v
+++ Gate_level/Binary_gray.v
@@ -1,7 +1,17 @@
+//without using vector 
 module b2g(G2,G1,G0,C,B,A);
   input C,B,A;
   output G2,G1,G0;
   buf b(G2,C);
   xor r1(G1,B,C);
   xor r2(G0,A,B);
+endmodule
+
+//Using vector 
+module b2g(g,b);
+  input [2:0]b;
+  output [2:0]g;
+  buf b1(g[2],b[2]);
+  xor r1(g[1],b[1],b[2]);
+  xor r2(g[0],b[0],b[1]);
 endmodule
--- Gate_level/Binary_gray_tb.v
+++ Gate_level/Binary_gray_tb.v
@@ -1,3 +1,4 @@
+//Without using vector
 module b2g_test;
   reg C,B,A;
   wire G2,G1,G0;
@@ -20,5 +21,29 @@ module b2g_test;
       $dumpfile("dum.vcd");
       $dumpvars(0,C,B,A,G2,G1,G0);
     end
+endmodule
+
+//Using vector
+module b2g_test;
+  reg [2:0]b;
+  wire [2:0]g;
+  b2g dut(g,b);
+  initial begin
+    b=3'b000;
+    #2 b=3'b001;
+    #2 b=3'b010;
+    #2 b=3'b011;
+    #2 b=3'b100;
+    #2 b=3'b101;
+    #2 b=3'b110;
+    #2 b=3'b111;  
+  end
+  initial begin
+    $monitor("sim time=%0t,b=%b,g=%b",$time,b,g);
+  end
+  initial begin 
+    $dumpfile("dum.vcd");
+    $dumpvars(0,b,g);
+  end
 endmodule
     
