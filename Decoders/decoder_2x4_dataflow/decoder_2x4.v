be5477649445001e8ed17ba1547c7550fce6805c
Harshitha Keshav <harshithakeshav909@gmail.com>
2026-05-16 12:01:06 +0000

    verilog

--- Decoders/decoder_2x4_dataflow/decoder_2x4.v
+++ Decoders/decoder_2x4_dataflow/decoder_2x4.v
@@ -1,11 +1,10 @@
-module decoder(O,I);
+//Data Flow
+module decoder1(I,O);
   input [1:0]I;
   output [3:0]O;
-  wire [0:1]w;
-  assign w[0]=~I[0];
-  assign w[1]=~I[1];
-  assign O[3]=w[0]&w[1];
-  assign O[1]=w[0]&I[1];
-  assign O[2]=I[0]&w[1];
-  assign O[0]=I[0]&I[1];
+  assign O[0]=~I[1]&~I[0];
+  assign O[1]=~I[1]& I[0];
+  assign O[2]= I[1]&~I[0];
+  assign O[3]= I[1]& I[0];
+  
 endmodule
--- Decoders/decoder_2x4_dataflow/decoder_2x4_tb.v
+++ Decoders/decoder_2x4_dataflow/decoder_2x4_tb.v
@@ -1,18 +1,17 @@
-module decoder_test;
+module decoder_tb;
   reg [1:0]I;
   wire [3:0]O;
-  decoder dut(O,I);
-  initial begin
-    I=2'b00;
-    #2 I=2'b01;
-    #2 I=2'b10;
-    #2 I=2'b11;
-  end
-  initial begin
-    $monitor("sim time=%0t,I=%b,O=%b",$time,I,O);
-  end
-  initial begin
-    $dumpfile("dum.vcd");
-    $dumpvars(0,I,O);
-  end
+  decoder dut(I,O);
+  initial 
+    begin
+      I = 2'b00;
+      #2 I = 2'b01;
+      #2 I = 2'b10; 
+      #2 I = 2'b11; 
+      
+    end
+  initial 
+    begin
+      $monitor("sim time=%0t,I=%b,O=%b",$time,I,O);
+    end
 endmodule
