cccebb99120dab9a40fef68b539235060c418c4e
Harshitha Keshav <harshithakeshav909@gmail.com>
2026-08-29 16:53:34 +0000

    verilog

---
+++ FSM/Melay_100/Melay_100_tb.v
@@ -0,0 +1,38 @@
+module tb;
+
+  reg clk,rst,seq_in;
+  wire det_out;
+
+  fsm_melay_100 dut (clk,rst,seq_in,det_out);
+
+  initial 
+    begin
+      clk=0;
+      rst=0;
+      seq_in=0;
+
+      #12 rst=1;
+
+      // 100
+      #10 seq_in=1;
+      #10 seq_in=0;
+      #10 seq_in=0;
+
+      #20 $finish;
+    end
+
+  always #5 clk=~clk;
+
+  initial
+    begin
+      $monitor("time=%0t clk=%b rst=%b seq_in=%b det_out=%b",
+               $time,clk,rst,seq_in,det_out);
+    end
+
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(0,tb);
+    end
+
+endmodule
